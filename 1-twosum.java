bb93cea3d8174c018a3bc8b1b5be15b2323050d5
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2024-12-20 21:31:27 +0900

    Update 1-twosum.java

--- 1-twosum.java
+++ 1-twosum.java
@@ -1,5 +1,24 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for(int i = 0; i < nums.length -1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    answer[0] = i;
+                    answer[1]= j;
+                    return answer;
+                }
+            }
+        }
+        return new int[0];
+    }
+    
+}
+
+// I writed the code but it is not efficiency
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
       // make a hashMap <Key, Value>
         Map<Integer, Integer> complements = new HashMap<>();
       // iterate the loops
