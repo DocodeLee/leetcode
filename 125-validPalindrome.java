b8fe3f29c5e2308929a187a8c111336e2fc7afe7
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-01-18 11:36:40 +0800

    Update 125-validPalindrome.java

--- 125-validPalindrome.java
+++ 125-validPalindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]",""); // change string to lower case
+        s = s.toLowerCase().replaceAll("[^a-z0-9]",""); // change string to lower case > replace all to blank if it is not a-z0-9
+        // ^ in first parentheses is negative if it is not a-z0-9 return to ""
         int left = 0; // setting point index 1
         int right = s.length() -1; // setting point index 2
 
