753ae142fc514bdd26f9c1fa5e0f74710e4aaf3b
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-03 22:50:57 +0900

    Update 26-Remove Duplicates.java

--- 26-Remove Duplicates.java
+++ 26-Remove Duplicates.java
@@ -1,18 +1,24 @@
-class Solution {
+class Solution { // TIme O(n) > pointer  Space O(1) > do not store
     public int removeDuplicates(int[] nums) {
-      //set pointer
-        int j = 1;
-      // iterate loops
-        for(int i = 1; i < nums.length; i++){
-          // if the new number given  
-          if(nums[i] != nums[i - 1]){
-              //change the index for the new number  
-              nums[j] = nums[i];
-              // plus pointer until the new number appears 
-                j++;
+        // array is sorted decreasing order > same number will be next
+        // from array we need to remove the duplicate > but do not need to change length
+
+        // return the unique number and return the number of unique numbers
+        // input size is ten thousands, and could be negative value
+
+        // need to compare the values > how about two pointer
+        if(nums.length == 0) return 0; // side case
+
+        int i = 1; // why we start from 1 ? > the 0 index is always unique so this is optimization
+
+        for(int j = 1; j < nums.length; j++){ //iterate
+            if(nums[j] != nums[i-1]){ //[i-1] > start from 1 but need to check 0 and 1 is different or not
+                // if new value founded
+                nums[i] = nums[j]; // change with i index
+                i++; // move i to next
             }
         }
-        return j;
-    }
+        return i; 
 
+    }
 }
