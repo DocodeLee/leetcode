977fac42ab8123eeedcc6e29e44975eea5361126
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-05 00:02:13 +0900

    Update 27-removeElement.java

--- 27-removeElement.java
+++ 27-removeElement.java
@@ -14,3 +14,27 @@ class Solution {
         return index;
     }
 }
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // arrays and int given
+        // going to remove the val value
+        // input and size is not that big
+        // return int as not same with val
+
+        // time : O(n) > Space O(1)
+        int i = 0;
+        // In here i need to think about why i initiate i >> as loop proceed i need to track
+
+        for(int k =0; k < nums.length; k++){ // iterate the array
+            if(val != nums[k]){ // if there is new value
+                nums[i] = nums[k]; // change it with the new things
+                i++; // i moves to the next and it will be the new length 
+            }
+        }
+
+        return i;
+
+
+    }
+}
