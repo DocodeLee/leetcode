e493a2b935d721cbc895c90785f2b8df2347bfc9
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-05 22:43:44 +0900

    Update 35-SearchInsertPosition.java

--- 35-SearchInsertPosition.java
+++ 35-SearchInsertPosition.java
@@ -2,6 +2,35 @@
 // ex > [1,3,5,6] if target is 5 return 2
 // if target is 2 return 1;
 
+
+// I thougth these
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // we will get sorted array and need to find target
+        // return the index if the target is founded, but no found the index where it would be
+        // algorithm should be O(log n)
+
+        int i = 0; // set int to return index
+        for( int num : nums){
+            
+            if(num < target){ // if num is small increase the index
+                i++;
+            }
+            if(num == target) {
+                return i; // if target found return i
+            }
+            if(num > target){
+                return i; // and if the 
+            }
+            i++;
+        }
+            return i;
+    }
+}
+
+
+// answer
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int index = 0; // set the default index
@@ -12,5 +41,22 @@ class Solution {
         }
 
         return index;
+    }
+}
+
+// answer 2
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // we will get sorted array and need to find target
+        // return the index if the target is founded, but no found the index where it would be
+        // algorithm should be O(log n)
+
+        int i = 0; // activate int i to track the index
+        
+        while(i < nums.length && nums[i]<target){ // if i is less than length, number is less than target
+            
+            i++; // increase i 
+        }
+        return i; // return i
     }
 }
