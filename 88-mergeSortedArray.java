366333e6143edfbe21c8d14abeef2561038dec5a
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-07 22:00:59 +0900

    Update 88-mergeSortedArray.java

--- 88-mergeSortedArray.java
+++ 88-mergeSortedArray.java
@@ -15,31 +15,30 @@ class Solution {
 // this method is not good because we put the variable and sort. time efficiency is low
 
 // Using Pointers
-
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // we are going to get two arrays
+        // m and n means the num of elements which will be merged
+        //final array.length should be m+n
 
-        //setting 3 pointers
-        int p = m+n -1; // p is the last index of nums1
-        int p1 = m -1; // p1 is where the last value in nums1
-        int p2 = n -1; // p2 is where the nums2 index
+        //but instead be stored inside the array nums1
 
-        // iterate until the index comes to null 
-        while(p1 >= 0 && p2 >= 0){
-            if(nums1[p1] > nums2[p2]){ // if nums1 value is bigger than nums2 value
-                nums1[p] = nums1[p1]; // change the nums1 value to the last index
-                p1--; // move pointer
-                p--; //move pointer
-            }else{ // when the nums2 value is bigger
-                nums1[p] = nums2[p2]; // change [p] value to nums2[p2]
-                p2--; // move pointers
-                p--; // move pointers
-            }
+        // m could be 0
+
+        // wants to use two pointer > because we merge, and need to be sorted (need to be compare)
+
+        // time complexity O(m+n) , space complexity O(1)
+
+        int i = m - 1; //need to -1 because index starts from 0
+        int j = n - 1;
+        int k = m + n - 1; //tracker for total length
+
+        while(j >=0){ // if j comes 0 no more elements to merege
+        if( i >= 0 && nums1[i] > nums2[j]){
+            nums1[k--] = nums1[i--];
+        }else{
+            nums1[k--] = nums2[j--];
         }
-        while(p2 >= 0 ){ // this is for case when the only nums2 has value
-            nums1[p] = nums2[p2]; // put the nums2[p2] into nums1
-            p2--; // move pointers
-            p--; // move pointers
         }
     }
 }
