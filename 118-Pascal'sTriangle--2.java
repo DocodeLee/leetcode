75f1a355c37daae5ceaa1daf50b9c2e6deadfe9a
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-08 22:14:07 +0900

    Update 118-Pascal'sTriangle--2.java

--- 118-Pascal'sTriangle--2.java
+++ 118-Pascal'sTriangle--2.java
@@ -5,31 +5,39 @@ Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-      
-        List<List<Integer>> triangle = new ArrayList<>(); // make List of Integer List
 
-        if(numRows ==0) return triangle; // edge case when the numRows is 0
-        // normal case
-        List<Integer> first_row = new ArrayList<>(); // make first Row
-        first_row.add(1); // add 1 into arrayList
-        triangle.add(first_row); // add the first list to the triangle
+        //first make a return Type because we don't have nothing
+        List<List<Integer>> result = new ArrayList<>();
 
-        // from the second row
-        for(int i =1; i<numRows; i++){ //this loop will be stop at 4 but we add (1) manually under/ so i< numRows doesn't matter
-            List<Integer> prev_row= triangle.get(i-1); // need prev_row info because we add the prev_row int to row
-            List<Integer> row = new ArrayList<>(); // make new row as ArrayList
+        //if the numRows are 0
+        if(numRows == 0) return result;
 
-            row.add(1); // first element is always 1
+        //from normal
+        // make a inner List<Intger>
+        List<Integer> firstRow = new ArrayList<>();
+        //the first row should be start from 1
+        firstRow.add(1);
+        result.add(firstRow);
 
-            // inner for loop
-            for(int j=1; j<i; j++){
-                row.add(prev_row.get(j-1)+prev_row.get(j)); // sum of prev_row[j-1] and prev_row[j] will be the element 
+        //from the secondRow
+        for(int i =1; i < numRows; i++){
+            List<Integer> prev_row = result.get(i-1); //call the previous Row
+            List<Integer> current_row = new ArrayList<>();
+
+            current_row.add(1);
+
+            //for other elements in inner ArrayList
+            for(int j =1; j < i; j++){
+                current_row.add(prev_row.get(j-1) + prev_row.get(j)); 
+                //add the numbers 
             }
 
-            row.add(1); // add 1 at last
-            triangle.add(row); // add this row to triangle
+            //after all loop add 1 to the last
+            current_row.add(1);
+            result.add(current_row);
         }
-        return triangle;
+        return result;
+        
     }
 }
 
