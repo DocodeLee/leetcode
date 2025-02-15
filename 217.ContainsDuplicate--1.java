e3e0daadd8552f125399548f151cdca985158d6c
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-15 19:30:01 +0900

    Update 217.ContainsDuplicate--1.java

--- 217.ContainsDuplicate--1.java
+++ 217.ContainsDuplicate--1.java
@@ -3,6 +3,8 @@ class Solution {
         //input int array, output boolean
         //checking through array? > HashSet or HashMap
 
+        //time O(n) space O(n)
+        
         Set<Integer> m = new HashSet<>(); //we don't need to count so just use HashSet
 
         for(int num : nums){ //iteration
@@ -15,3 +17,34 @@ class Solution {
         return false;
     }
 }
+
+// method 2 
+
+// time O(n log(n) ) space O(n)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+//method 3
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        
+        for(int num : nums){
+            numSet.add(num);
+        }
+
+        return numSet.size() < nums.length;
+    }
+}
+
