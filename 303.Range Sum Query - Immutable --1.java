101c0cbe2a5046ba2cd6fa913be40713bac00e77
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-21 23:44:04 +0900

    Create 303.Range Sum Query - Immutable --1.java

---
+++ 303.Range Sum Query - Immutable --1.java
@@ -0,0 +1,21 @@
+
+
+class NumArray {
+    int[] res;
+
+    public NumArray(int[] nums) {
+        // initialize runngin sum array
+        this.res = new int[nums.length];
+        this.res[0] = nums[0];
+
+        // compute running sum
+        for(int i = 1; i < nums.length; i++){
+            this.res[i] = this.res[i - 1] + nums[i];
+        }
+
+    }
+    
+    public int sumRange(int left, int right) {
+        return left > 0 ? this.res[right] - this.res[left -1] : this.res[right];
+    }
+}
