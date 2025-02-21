d030b8ac4172d31d8d23f100acc3607c3aea78c4
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-22 00:25:10 +0900

    Update 303.Range Sum Query - Immutable --1.java

--- 303.Range Sum Query - Immutable --1.java
+++ 303.Range Sum Query - Immutable --1.java
@@ -1,21 +1,23 @@
 
 
 class NumArray {
-    int[] res;
+    int[] res; // declare new instance
 
-    public NumArray(int[] nums) {
+    public NumArray(int[] nums) { // constructor which get arrays
         // initialize runngin sum array
-        this.res = new int[nums.length];
-        this.res[0] = nums[0];
+        this.res = new int[nums.length];  // make a new int[] with nums.length
+        this.res[0] = nums[0]; //set the first index as 0;
 
         // compute running sum
         for(int i = 1; i < nums.length; i++){
-            this.res[i] = this.res[i - 1] + nums[i];
+            this.res[i] = this.res[i - 1] + nums[i]; // the [i] value will be the sum of [i-1] and [i]
         }
 
     }
     
-    public int sumRange(int left, int right) {
-        return left > 0 ? this.res[right] - this.res[left -1] : this.res[right];
+    public int sumRange(int left, int right) { // method sumrange
+        return left > 0 ? this.res[right] - this.res[left -1] : this.res[right]; 
+        // if left is bigger than 0 rigth should be gbigger than 0
+        // if left is 0 we need to return the [right] 
     }
 }
