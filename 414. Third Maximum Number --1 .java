7151b8aec66340e948487acfe5d0c38e222cebe2
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-02-24 23:26:08 +0900

    Update 414. Third Maximum Number --1 .java

--- 414. Third Maximum Number --1 .java
+++ 414. Third Maximum Number --1 .java
@@ -18,3 +18,27 @@ class Solution {
         return Collections.max(s);
     }
 }
+
+// Faster method
+class Solution {
+    public int thirdMax(int[] nums) {
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+        for(int num : nums){
+            if(num > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }else if(max1 > num && max2 < num){
+                max3 = max2;
+                max2 = num;
+            }else if (max2 > num && max3 < num){
+                max3 = num;
+            }
+        }
+        return max3 != Long.MIN_VALUE ? (int) max3 : (int) max1;
+
+    }
+}
