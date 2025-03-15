4275c697514d7515a18b743753dbed38520fd9b0
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-03-15 20:54:48 +0900

    Update 575.DistributeCandies--1.java

--- 575.DistributeCandies--1.java
+++ 575.DistributeCandies--1.java
@@ -20,3 +20,21 @@ class Solution {
 }
 
 // very unefficient
+
+// use type.size() not to count up the arrays 
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        // check length and count how many candy eat
+        // this is not necessary
+        
+        // put it into map and if there is something
+        // count up
+
+        Set<Integer> type = new HashSet<>();
+
+        for(int candy : candyType){
+            type.add(candy);
+        }
+        return Math.min(type.size(), candyType.length/2);
+    }
+}
