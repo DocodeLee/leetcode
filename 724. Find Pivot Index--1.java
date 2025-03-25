dc15275226324886b9ec3ad04fb3d8b7a6cbd4dd
Lee Hyunil <167724096+DocodeLee@users.noreply.github.com>
2025-03-25 23:08:24 +0900

    Create 724. Find Pivot Index--1.java

---
+++ 724. Find Pivot Index--1.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        //need to check array sum
+        // first one : 0 & 0,1  second one : length-1 & length-1, length-2
+      
+        int rightSum = 0;
+        int leftSum = 0;
+      
+        for (int i =0; i < nums.length; i++){
+            rightSum += nums[i]; // sum the all the numbers
+        }
+      
+        for(int i = 0; i < nums.length; i++){
+            rightSum -= nums[i]; // it makes leftSum bigger and naturally decrease length
+            if(leftSum == rightSum) {
+                return i; // if there is same return i
+            }
+            leftSum += nums[i]; // increase leftSum
+        }
+        return -1;
+    }
+}
